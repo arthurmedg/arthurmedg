40b92e52b263b58e953eb06b0f8b3d545195f78e
medina <arthurmedg@gmail.com>
2026-02-02 08:31:50 -0300

    Add files via upload

---
+++ revisaoAula1/script.js
@@ -0,0 +1,13 @@
+// for(i=5;i>-1;i--){
+//     if(i == 0){
+//         console.log("Feliz Ano Novo!")
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+let num = Number(prompt("Escolha o número da tabuada"))
+
+for(i=1;i<=10;i++){
+    console.log(`${num} x ${i} = ${i * num}`)
+}
